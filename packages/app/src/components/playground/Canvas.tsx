d7a9107c988c7174d45ffdbeb530034341873f96
Sai Palepu <saikrishnatejausait@gmail.com>
2024-08-23 09:09:50 -0500

    feat: open modal on edge connection

--- packages/app/src/components/playground/Canvas.tsx
+++ packages/app/src/components/playground/Canvas.tsx
@@ -1,4 +1,10 @@
-import React, { useState, useCallback, useEffect, SetStateAction, Dispatch } from 'react';
+import React, {
+  useState,
+  useCallback,
+  useEffect,
+  SetStateAction,
+  Dispatch,
+} from 'react';
 import { useDrop } from 'react-dnd';
 import { ReactSVG } from 'react-svg';
 import HighlightOffIcon from '@material-ui/icons/HighlightOffSharp';
@@ -22,25 +28,24 @@ import CustomEdge from './CustomEdge';
 interface CustomNodeProps {
   id: string;
   data: {
-    label: string,
-    onRemove: () => {},
-    onOpen: () => (newItem: any) => {},
-    handleRemoveItem: () => {},
+    label: string;
+    onRemove: () => {};
+    onOpen: () => (newItem: any) => {};
+    handleRemoveItem: () => {};
   };
-  handleRemoveItem: (id: string) => {}
+  handleRemoveItem: (id: string) => {};
 }
 
 interface CanvasProps {
-  items: any[],
-  setItems: Dispatch<SetStateAction<never[]>
+  items: any[];
+  setItems: Dispatch<SetStateAction<never[]>>;
 }
-
 
 const ItemType = {
   TOOL: 'tool',
 };
 
-const iconMapping: {[key: string]: string} = {
+const iconMapping: { [key: string]: string } = {
   'ECS-Cluster': 'icons/ECS-Cluster.svg',
   'ECS-Service': 'icons/ECS-Service.svg',
   'ECS-Task-Definition': 'icons/ECS-Task-Definition.svg',
@@ -90,7 +95,9 @@ const CustomNode = ({ id, data, handleRemoveItem }: CustomNodeProps) => {
 };
 
 const nodeTypes = {
-  custom: (props: React.JSX.IntrinsicAttributes & CustomNodeProps) => <CustomNode {...props} handleRemoveItem={props.data.handleRemoveItem} />,
+  custom: (props: React.JSX.IntrinsicAttributes & CustomNodeProps) => (
+    <CustomNode {...props} handleRemoveItem={props.data.handleRemoveItem} />
+  ),
 };
 
 export const Canvas = ({ items, setItems }: CanvasProps) => {
@@ -99,6 +106,13 @@ export const Canvas = ({ items, setItems }: CanvasProps) => {
   const [inputValues, setInputValues] = useState({});
   const [variableData, setVariableData] = useState({ inputs: {}, outputs: {} });
   const [error, setError] = useState(null);
+  const [connectModalOpen, setConnectModalOpen] = useState(false);
+  const [connectionInfo, setConnectionInfo] = useState({
+    source: '',
+    target: '',
+    sourceName: '',
+    targetName: '',
+  });
   const configApi = useApi(configApiRef);
   const backendBaseUrl = configApi.getString('backend.baseUrl');
 
@@ -121,49 +135,84 @@ export const Canvas = ({ items, setItems }: CanvasProps) => {
       },
       type: 'custom',
     }));
-  
+
     setNodes(newNodes as any);
-  
+
     const newEdges = items.flatMap((item: any) =>
       (item.connections || [])
-        .filter((conn: { target: string; }) => conn.target !== item.id)
-        .map((conn: { target: string; }) => ({
+        .filter((conn: { target: string }) => conn.target !== item.id)
+        .map((conn: { target: string }) => ({
           id: `${item.id}-${conn.target}`,
           type: 'custom-edge',
           source: item.id,
           target: conn.target,
           sourceHandle: 'source',
           targetHandle: 'target',
-          animated: true
-        }))
+          animated: true,
+        })),
     );
 
-    const distinctEdges = Array.from(new Set(newEdges.map(edge => JSON.stringify(edge)))).map(e => JSON.parse(e));
-  
+    const distinctEdges = Array.from(
+      new Set(newEdges.map(edge => JSON.stringify(edge))),
+    ).map(e => JSON.parse(e));
+
     setEdges(distinctEdges);
   }, [items, setNodes, setEdges]);
 
   const handleConnect = useCallback(
-    (params) => {
-      setEdges((eds) => addEdge({ ...params, sourceHandle: 'source', targetHandle: 'target', animated: true, type: 'custom-edge' }, eds));
-      
+    params => {
+      setEdges(eds =>
+        addEdge(
+          {
+            ...params,
+            sourceHandle: 'source',
+            targetHandle: 'target',
+            animated: true,
+            type: 'custom-edge',
+          },
+          eds,
+        ),
+      );
+
       const { source, target } = params;
-      setItems((items) => 
-        items.map((item) => {
+      setItems(items =>
+        items.map(item => {
           if (item.id === source || item.id === target) {
-            const updatedConnections = (item.connections || []).concat({ source, target });
+            const updatedConnections = (item.connections || []).concat({
+              source,
+              target,
+            });
             return { ...item, connections: updatedConnections };
           }
           return item;
-        })
+        }),
       );
     },
-    [setEdges, setItems]
+    [setEdges, setItems],
   );
 
+  const findItem = (itemId: string) => items.find(item => item.id === itemId);
+
   const onConnect = useCallback(
-    params => handleConnect(params),
-    [handleConnect]
+    params => {
+      handleConnect(params);
+
+      const sourceItem = findItem(params.source);
+      const targetItem = findItem(params.target);
+
+      if (sourceItem && targetItem) {
+        setConnectionInfo({
+          source: params.source,
+          target: params.target,
+          sourceName: sourceItem.name.replace(/-/g, ' '),
+          targetName: targetItem.name.replace(/-/g, ' '),
+        });
+        setConnectModalOpen(true);
+      } else {
+        console.warn('Source or target item not found in items array.');
+      }
+    },
+    [handleConnect, findItem],
   );
 
   const handleOpen = async item => {
@@ -214,6 +263,7 @@ export const Canvas = ({ items, setItems }: CanvasProps) => {
 
     setItems(updatedItems);
     handleClose();
+    setConnectionInfo(null);
   };
 
   const [{ isOver }, drop] = useDrop({
@@ -306,10 +356,10 @@ export const Canvas = ({ items, setItems }: CanvasProps) => {
         edgeTypes={edgeTypes}
       >
         <Background />
-        <Controls style={{position: 'absolute', bottom: '40px'}}/>
+        <Controls style={{ position: 'absolute', bottom: '40px' }} />
       </ReactFlow>
       <Modal open={open} onClose={handleClose} style={{ outline: 'none' }}>
-      <Box
+        <Box
           sx={{
             position: 'absolute',
             top: '50%',
@@ -378,6 +428,45 @@ export const Canvas = ({ items, setItems }: CanvasProps) => {
               style={{ marginTop: 16, marginLeft: 8 }}
             >
               Cancel
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+      <Modal
+        open={connectModalOpen}
+        onClose={() => setConnectModalOpen(false)}
+        style={{ outline: 'none' }}
+      >
+        <Box
+          sx={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            width: 600,
+            bgcolor: 'greentheme.green',
+            p: 4,
+            outline: 'none',
+            borderRadius: 16,
+          }}
+        >
+          <h2 style={{ color: theme.palette.greentheme.offwhite }}>
+            You are connecting {connectionInfo.sourceName} to{' '}
+            {connectionInfo.targetName}
+          </h2>
+          {/* <p style={{ color: theme.palette.greentheme.offwhite }}>
+            <strong>{connectionInfo.source}</strong> to{' '}
+            <strong>{connectionInfo.target}</strong>.
+          </p> */}
+          {/* Add any additional inputs or information you want here */}
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
+            <Button
+              onClick={() => setConnectModalOpen(false)}
+              variant="contained"
+              color="primary"
+              style={{ marginTop: 16 }}
+            >
+              Close
             </Button>
           </Box>
         </Box>
